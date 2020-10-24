237ba4b113f6712afa47448d23307ce09eaad2b2
Miral <miralmodi09@gmail.com>
2020-10-24 10:01:12 +0530

    [Miral]: Invoice test case minimum fare support.

--- CabInvoiceGenerator/src/test/java/com/capgemini/cabinvoicegenerator/InvoiceServiceTest.java
+++ CabInvoiceGenerator/src/test/java/com/capgemini/cabinvoicegenerator/InvoiceServiceTest.java
@@ -2,6 +2,8 @@ package com.capgemini.cabinvoicegenerator;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.Assert;
@@ -9,8 +11,7 @@ import junit.framework.Assert;
 
 public class InvoiceServiceTest 
 {
-    
-    @Test
+	@Test
    public void givenDistanceAndTime_ShouldReturnTotalFare() {
     	InvoiceGenerator  invoiceGenerator = new InvoiceGenerator();
     	double distance = 2.0;
@@ -18,4 +19,13 @@ public class InvoiceServiceTest
     	double fare = invoiceGenerator.calculateFare(distance, time);
     	Assert.assertEquals(25, fare, 0.0);
     }
+
+    public void giveFareFindTheMinimumFare(){
+		InvoiceGenerator  invoiceGenerator = new InvoiceGenerator();
+		double distance = 1;
+		int time = 1;
+		double fare =  invoiceGenerator.calculateFare(distance,time);
+		Assume.assumeTrue(5>fare);
+		//Now update the functionality to support minimum fare.
+	}
 }
